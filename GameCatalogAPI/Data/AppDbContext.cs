c3b2b0a11b55a2d8b3f7daf761004b7042a79d52
Zerg-link <ithhelluc@gmail.com>
2026-03-01 20:09:29 +0500

    Edited models and added AppDbContext.cs

---
+++ GameCatalogAPI/Data/AppDbContext.cs
@@ -0,0 +1,43 @@
+﻿// Data/AppDbContext.cs
+
+using GameCatalogAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameCatalogAPI.Data
+{
+    /// <summary>
+    /// Class describes how to connect, edit models with tables in database.
+    /// </summary>
+    public class AppDbContext : DbContext
+    {
+        /// <summary>
+        /// Table of games from DataBase.
+        /// </summary>
+        public DbSet<Game> Games { get; set; }
+
+        /// <summary>
+        /// Table of reviews from DataBase.
+        /// </summary>
+        public DbSet<Review> Reviews { get; set; }
+
+        /// <summary>
+        /// Table of platforms from DataBase.
+        /// </summary>
+        public DbSet<Platform> Platforms { get; set; }
+
+        /// <summary>
+        /// Table of genres from DataBase.
+        /// </summary>
+        public DbSet<Genre> Genres { get; set; }
+        
+
+        /// <summary>
+        /// Connection DataBase options to the DBContext.
+        /// </summary>
+        /// <param name="options"> DataBase information tables.</param>
+        public AppDbContext(DbContextOptions<AppDbContext> options) :base(options)
+        {
+
+        }
+    }
+}
